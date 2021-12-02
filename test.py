8cb7c3ec68a631a281060d0751120d3da8936b3e
andhika <siewom007@gmail.com>
2021-12-02 12:51:08 +0700

    update tes.py untuk percobaan

--- test.py
+++ test.py
@@ -3,9 +3,11 @@ import csv
 import pandas
 from weibull.weibull import Analysis
 
-data = pandas.read_csv(r'data1.csv', delimiter=',')
-data1 = pandas.DataFrame(data, columns=['TBF'])
-fail_times = data1
+data = pandas.read_csv(r'data1.csv')
+data1 = pandas.Series(data, index="TBF")
+bool(data1)
+fail_times = [data1]
+#fail_times = [40, 540, 639, 660, 765, 812, 845, 943, 1419, 2188, 3389, 4265, 4320, 4404, 5580, 7368, 10480, 14370, 21120, 35650, 37806, 38640, 50470]
 analysis = weibull.Analysis(fail_times, unit='hour')
 analysis.fit(method='mle')
 analysis.probplot()
