6b768219b0efa68aa50ad4d2630cb3786020a3c4
andhika <siewom007@gmail.com>
2021-12-09 08:45:50 +0700

    commit staged

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
-import weibull 
-import csv
+import weibull
+# import csv
 import pandas
-from weibull.weibull import Analysis
+# from weibull.weibull import Analysis
 
-data = pandas.read_csv(r'data1.csv')
-data1 = pandas.Series(data, index="TBF")
-bool(data1)
-fail_times = [data1]
-#fail_times = [40, 540, 639, 660, 765, 812, 845, 943, 1419, 2188, 3389, 4265, 4320, 4404, 5580, 7368, 10480, 14370, 21120, 35650, 37806, 38640, 50470]
-analysis = weibull.Analysis(fail_times, unit='hour')
+# with open('data1.csv', newline='') as f:
+#    g = csv.reader(f)
+#    fail_times = next(g)
+data = pandas.read_csv("data1.csv", )
+data = pandas.DataFrame(index=[10])
+print(data)
+# data2 = (data)
+# print(data)
+# data1 = pandas.DataFrame(data)
+# print(data1)
+# bool(data1)
+# fail_times = [data]
+# fail_times = [40, 540, 639, 660, 765, 812, 845, 943, 1419, 2188, 3389, 4265, 4320, 4404, 5580, 7368, 10480, 14370, 21120, 35650, 37806, 38640, 50470]
+analysis = weibull.Analysis(data, unit='hour')
 analysis.fit(method='mle')
 analysis.probplot()
