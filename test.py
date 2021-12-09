900e3c877a6a23663614ef4e9d2083e4b256d56a
andhika <siewom007@gmail.com>
2021-12-09 09:44:32 +0700

    tambah data 3+ ujicobatest.py

--- test.py
+++ test.py
@@ -6,16 +6,16 @@ import pandas
 # with open('data1.csv', newline='') as f:
 #    g = csv.reader(f)
 #    fail_times = next(g)
-data = pandas.read_csv("data1.csv", )
-data = pandas.DataFrame(index=[10])
-print(data)
+data = pandas.read_csv("data3_noeditedit.csv", )
+# data = pandas.DataFrame(index=[10])
+# print(data)
 # data2 = (data)
 # print(data)
 # data1 = pandas.DataFrame(data)
 # print(data1)
 # bool(data1)
 # fail_times = [data]
-# fail_times = [40, 540, 639, 660, 765, 812, 845, 943, 1419, 2188, 3389, 4265, 4320, 4404, 5580, 7368, 10480, 14370, 21120, 35650, 37806, 38640, 50470]
+#fail_times = [40, 540, 639, 660, 765, 812, 845, 943, 1419, 2188, 3389, 4265, 4320, 4404, 5580, 7368, 10480, 14370, 21120, 35650, 37806, 38640, 50470]
 analysis = weibull.Analysis(data, unit='hour')
 analysis.fit(method='mle')
 analysis.probplot()
